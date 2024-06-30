b92ba34f370b79bc0d56be75849c1b2bb1be64ed
hraki1 <ahmadhraki752@gmail.com>
2024-06-30 13:15:22 +0300

    d

---
+++ Task-2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace PorjectOne
+{
+    class Program
+    {
+
+        void SumAvarage(int[] nums)
+        {
+            int sum = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                sum += nums[i];
+                
+            }
+            int ava = 0;
+            ava = sum / nums.Length;
+
+            Console.WriteLine($"The Sum of 10 Numbers is :{sum}");
+            Console.WriteLine($"The Average is :{ava}");
+        }
+
+        static void Main(string[] args)
+        {
+            Program ojb = new();
+            int[] nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+            ojb.SumAvarage(nums);
+
+
+        }
+
+    }
+}
+
